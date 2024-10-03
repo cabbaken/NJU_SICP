b782cc0453b4b07d740f79e6a8a970d570085d13
cabbaken <cabbaken@outlook.com>
2024-10-03 12:49:56 +0800

    first commit

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,75 @@
+def both_odd(a, b):
+    """Returns True if both a and b are odd numbers.
+
+    >>> both_odd(-1, 1)
+    True
+    >>> both_odd(2, 1)
+    False
+    """
+    return a%2==1 and b % 2 == 1 # You can replace this line!
+
+
+def factorial(n):
+    """Return the factorial of a positive integer n.
+
+    >>> factorial(3)
+    6
+    >>> factorial(5)
+    120
+    """
+    a,res=1,1
+    while a!=n:
+        res*=a
+        a+=1
+    return res*n
+
+
+def is_triangle(a, b, c):
+    """Given three integers (may be nonpositive), judge whether the three
+    integers can form the three sides of a triangle.
+
+    >>> is_triangle(2, 1, 3)
+    False
+    >>> is_triangle(5, -3, 4)
+    False
+    >>> is_triangle(2, 2, 2)
+    True
+    """
+    return a+b>c and a+c>b and b+c>a
+
+
+def number_of_six(n):
+    """Return the number of 6 in each digit of a positive integer n.
+
+    >>> number_of_six(666)
+    3
+    >>> number_of_six(123456)
+    1
+    """
+    res=0
+    while n:
+        if n%10==6:
+            res+=1
+        n//=10
+    return res
+
+
+def max_digit(x):
+    """Return the max digit of x.
+
+    >>> max_digit(10)
+    1
+    >>> max_digit(4224)
+    4
+    >>> max_digit(1234567890)
+    9
+    >>> # make sure that you are using return rather than print
+    >>> a = max_digit(123)
+    >>> a
+    3
+    """
+    res=x%10
+    while x:
+        res=max(res,x%10)
+        x//=10
+    return res
---
+++ lab/lab03/lab03.py
@@ -0,0 +1,159 @@
+""" Lab 3: Recursion and Tree Recursion """
+
+this_file = __file__
+
+
+def skip_add(n):
+    """ Takes a number n and returns n + n-2 + n-4 + n-6 + ... + 0.
+
+    >>> skip_add(5)  # 5 + 3 + 1 + 0
+    9
+    >>> skip_add(10) # 10 + 8 + 6 + 4 + 2 + 0
+    30
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'skip_add',
+    ...       ['While', 'For'])
+    True
+    """
+    if n>0:
+        return n+skip_add(n-2)
+    return 0
+
+
+def summation(n, term):
+
+    """Return the sum of the first n terms in the sequence defined by term.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(this_file, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    if n-1>0:
+        return term(n)+summation(n-1,term)
+    return term(n)
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    if a<b:
+        a,b=b,a
+    if b==0:
+        return a
+    return gcd(b,a%b)
+    
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    res=0
+    if m!=1:
+        res+=paths(n,m-1)
+    if n!=1:
+        res+=paths(n-1,m)
+    if n==1 and m==1:
+        return 1
+    return res
+
+def max_subseq(n, l):
+    """
+    Return the maximum subsequence of length at most l that can be found in the given number n.
+    For example, for n = 20125 and l = 3, we have that the subsequences are
+        2
+        0
+        1
+        2
+        5
+        20
+        21
+        22
+        25
+        01
+        02
+        05
+        12
+        15
+        25
+        201
+        202
+        205
+        212
+        215
+        225
+        012
+        015
+        025
+        125
+    and of these, the maximum number is 225, so our answer is 225.
+
+    >>> max_subseq(20125, 3)
+    225
+    >>> max_subseq(20125, 5)
+    20125
+    >>> max_subseq(20125, 6) # note that 20125 == 020125
+    20125
+    >>> max_subseq(12345, 3)
+    345
+    >>> max_subseq(12345, 0) # 0 is of length 0
+    0
+    >>> max_subseq(12345, 1)
+    5
+    """
+    d=[c for c in str(n)]
+    res=-1
+    v=[]
+    if l>=len(d):
+        return n
+    if l==0:
+        return 0
+
+    def to_int(v):
+        return int(''.join(map(str,v)))
+
+    def cal(v,now,depth):
+        nonlocal d,l,res
+        if(depth==l):
+            res=max(res,to_int(v))
+            return
+        while now+1 < len(d)-(l-depth-1):
+            now+=1
+            depth+=1
+            v.append(d[now])
+            cal(v,now,depth)
+            v.pop()
+            depth-=1
+
+    cal(v,-1,0)
+    return res
